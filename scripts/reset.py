d2eb1215375788413bfb7d19fd2a8099497706a8
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-07-02 09:52:47 -0300

    updated db init script to add benchmark - course relationships

--- scripts/reset.py
+++ scripts/reset.py
@@ -17,16 +17,21 @@ with open(os.path.join(directory, 'data.json')) as fp:
     courses = json.load(fp)
 courses = list(courses.values())[:15]
 
+collection = db.benchmarks
+benchmarks = ['Benchmark 1.0', 'Benchmark 1.1', 'Benchmark 1.2', 'Benchmark 1.3', 'Benchmark 2.0']
+benchmarks = [{'name': name} for name in benchmarks]
+ids = collection.insert_many(benchmarks).inserted_ids
+assert collection.count() == len(benchmarks)
+print('Inserted {} benchmarks'.format(len(benchmarks)))
+
+print('Adding test benchmarks!')
+for course, benchmark_id in zip(courses, ids):
+    course['benchmarks'] = [benchmark_id]
+courses[0]['benchmarks'].append(ids[1])
+
 collection = db.courses
 collection.insert_many(courses)
 assert collection.count() == len(courses)
 print('Inserted {} courses!'.format(len(courses)))
-
-collection = db.benchmarks
-benchmarks = ['Benchmark 1.0', 'Benchmark 1.1', 'Benchmark 1.2', 'Benchmark 1.3', 'Benchmark 2.0']
-benchmarks = [{'name': name} for name in benchmarks]
-collection.insert_many(benchmarks)
-assert collection.count() == len(benchmarks)
-print('Inserted {} benchmarks'.format(len(benchmarks)))
 
 print('Finished')
